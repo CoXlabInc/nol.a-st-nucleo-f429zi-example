552c59251f85e09f81f96952117cf006defe5443
Jongsoo Jeong <jsjeong@coxlab.kr>
2017-12-11 16:30:52 +0900

    CS-241 Added entering Under-drive STOP mode when the system in idle.

--- Basic/main.cpp
+++ Basic/main.cpp
@@ -1,5 +1,8 @@
 #include <cox.h>
 
+/* In order to measure current in idle mode, define MEASURE_CURRENT_IN_SLEEP below. */
+#define MEASURE_CURRENT_IN_SLEEP
+
 Timer timerHello;
 
 static void taskHello(void *) {
@@ -7,8 +10,11 @@ static void taskHello(void *) {
   Serial.println("Serial) Hello World!");
   Serial2.println();
   Serial2.println("Serial2) Hello World!");
+
+#ifndef MEASURE_CURRENT_IN_SLEEP
   SerialUSB.println();
   SerialUSB.printf("SerialUSB) Hello World!\n");
+#endif
   digitalToggle(PB14);
 }
 
@@ -32,17 +38,23 @@ void setup() {
   Serial.begin(115200);
   Serial.println("*** [ST Nucleo-F429ZI] Basic Functions ***");
   Serial.onReceive(eventSerialRx);
+#ifndef MEASURE_CURRENT_IN_SLEEP
   Serial.listen();
+#endif
 
   Serial2.begin(115200);
   Serial2.println("*** [ST Nucleo-F429ZI] Basic Functions ***");
   Serial2.onReceive(eventSerialRx);
+#ifndef MEASURE_CURRENT_IN_SLEEP
   Serial2.listen();
+#endif
 
+#ifndef MEASURE_CURRENT_IN_SLEEP
   SerialUSB.begin();
   SerialUSB.println("*** [ST Nucleo-F429ZI] Basic Functions ***");
   SerialUSB.onReceive(eventSerialRx);
   SerialUSB.listen();
+#endif
 
   timerHello.onFired(taskHello, NULL);
   timerHello.startPeriodic(1000);
